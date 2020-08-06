34a493d1c56261277939571aa4b620e150531d2c
kouki <kouworld0123@gmail.com>
2020-08-07 02:54:46 +0900

    feat: ログイン・ログアウト，idTokenの表示まで

--- iOSFirebaseAuth/ContentView.swift
+++ iOSFirebaseAuth/ContentView.swift
@@ -7,10 +7,56 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    let userId: String = "kmdkuk@example.com"
+    let password: String = "password"
+    var test: String = "a"
+    @State var user: User!
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Button(action: {
+                print("Button Tapped")
+                print("userId = \(self.userId)")
+                print("password = \(self.password)")
+                print("以上の情報でログインします．")
+                Auth.auth().signIn(withEmail: self.userId, password: self.password) { authResult, error in
+                  // ...
+                    print(authResult ?? "none")
+                    self.user = Auth.auth().currentUser
+                }
+                let currentUser = Auth.auth().currentUser
+                // https://firebase.google.com/docs/auth/admin/verify-id-tokens?hl=ja#ios
+                currentUser?.getIDTokenForcingRefresh(true) { idToken, error in
+                    if error != nil {
+                    // Handle error
+                    return;
+                  }
+
+                  // Send token to your backend via HTTPS
+                  // ...
+                    print(idToken ?? "idToken is none")
+                }
+            }){
+                Text("ログイン")
+            }
+            
+            Button(action: {
+                print("Logout tapped")
+                do{
+                    try Auth.auth().signOut()
+                    self.user = Auth.auth().currentUser
+                }catch let signOutError as NSError {
+                    print ("Error signing out: %@", signOutError)
+                }
+            })
+            {
+                Text("ログアウト")
+            }
+            Text("uid=\(self.user?.uid ?? "none")")
+            Text("email=\(self.user?.email ?? "none")")
+        }
     }
 }
 
